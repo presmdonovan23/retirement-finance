7ffbfda986b83e8e48bf7a96e1244632b6fc5e20
Preston Donovan <prestonmdonovan@gmail.com>
2021-12-30 16:41:45 -0500

    Add validation for RetirementAnalysis inputs, fix portfolio.step bug.

--- refi/retirement_analysis.py
+++ refi/retirement_analysis.py
@@ -1,7 +1,7 @@
 class RetirementAnalysis:
     
-    def __init__(self, initial_age, retirement_age, death_age, portfolio, deferral_scenario, consumption_scenario, inflation_scenario, ssb_scenario):
-        
+    def __init__(self, initial_age, retirement_age, death_age, portfolio, deferral_scenario, consumption_scenario, inflation_scenario, cpi_scenario, ssb_scenario):
+
         self.initial_age = initial_age
         self.retirement_age = retirement_age
         self.death_age = death_age
@@ -11,7 +11,26 @@ class RetirementAnalysis:
         self.deferral_scenario = deferral_scenario
         self.consumption_scenario = consumption_scenario
         self.inflation_scenario = inflation_scenario
+        self.cpi_scenario = cpi_scenario
         self.ssb_scenario = ssb_scenario
+
+        self._validate_initial_inputs()
+
+    def _validate_initial_inputs(self):
+        if self.deferral_scenario.period != -1:
+            raise ValueError('Cannot initialize RetirementAnalysis: deferral_scenario.period = {0}.'.format(self.deferral_scenario.period))
+
+        if self.ssb_scenario.period != -1:
+            raise ValueError('Cannot initialize RetirementAnalysis: deferral_scenario.period = {0}.'.format(self.ssb_scenario.period))
+
+        if self.inflation_scenario.period != -1:
+            raise ValueError('Cannot initialize RetirementAnalysis: inflation_scenario.period = {0}.'.format(self.inflation_scenario.period))
+
+        if self.cpi_scenario.period != -1:
+            raise ValueError('Cannot initialize RetirementAnalysis: cpi_scenario.period = {0}.'.format(self.cpi_scenario.period))
+
+        if self.consumption_scenario.period != -1:
+            raise ValueError('Cannot initialize RetirementAnalysis: consumption_scenario.period = {0}.'.format(self.consumption_scenario.period))
 
     def simulate(self):
         
@@ -20,14 +39,15 @@ class RetirementAnalysis:
 
     def _step(self):
         self.inflation_scenario.step()
+        self.cpi_scenario.step()
         self.deferral_scenario.step()
         self.consumption_scenario.step()
         self.ssb_scenario.step()
 
-        deferral_amt = self.deferral_scenario.value
+        deposit_amt = self.deferral_scenario.value
         withdrawal_amt = self._get_withdrawal()
 
-        self.portfolio.step(deferral_amt=deferral_amt, withdrawal_amt=withdrawal_amt)
+        self.portfolio.step(deposit_amt=deposit_amt, withdrawal_amt=withdrawal_amt)
 
     def _get_withdrawal(self):
         return self.consumption_scenario.value - self.ssb_scenario.value
